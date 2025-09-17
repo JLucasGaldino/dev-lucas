abcc35adaa21323eda779303c0f53c868e15445d
JLucasGaldino <lucasgaldino@tuta.io>
2025-09-17 18:19:55 +0200

    Add github actions

--- astro.config.ts
+++ astro.config.ts
@@ -22,6 +22,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
+  site: 'https://jlucasgaldino.github.io',
+  base: '/dev-lucas',
   output: 'static',
 
   integrations: [
